a1df1f7818a27a2e97409bb55e38a420066a63fa
Scott Roberts <sroberts.kia@gmail.com>
2019-07-22 21:29:13 +0000

    Added test cases

--- head.js
+++ head.js
@@ -7,15 +7,22 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(array) {
-  if (array[0] === null) {
+  if (array.length === 0) {
     return undefined;
   } else {
     return array[0];
   }
 };
 
-let nullArr = [];
+
 // TEST CODE
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+//Tests a null array
+let nullArr = [];
 console.log(head(nullArr));
+
+//Tests an array with one element
+let oneElementArray = [33];
+console.log(head(oneElementArray));
