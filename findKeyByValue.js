8ecedfab2a78394575d2647fcf2008a1768a5ee5
Scott Roberts <sroberts.kia@gmail.com>
2019-07-24 16:31:00 +0000

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value){
+  let returnKey = undefined;
+
+  for(const key in obj){
+    if (obj.hasOwnProperty(key)) {
+      if(obj[key] === value){
+        returnKey = key;
+      }  
+    }
+  }
+  return returnKey;
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
