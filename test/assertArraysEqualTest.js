0000f4de38ac6181dd944836fbea1c55c259da42
Scott Roberts <sroberts.kia@gmail.com>
2019-07-29 19:47:57 +0000

    refactored tests to use mocha and chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -8,3 +8,5 @@ let arr3 = [0];
 assertArraysEqual(arr1, arr2);
 assertArraysEqual(arr1, arr3);
 
+//I didn't change these because they are virtually the same as the built in assert function
+//I figured asserting an assert was strange.
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -4,3 +4,6 @@ const result = ["Hello", "Lighthouse", "Labs"];
 assertEqual(result.length, 3); // ensure we get back two elements
 assertEqual(result[1], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[2], "Labs"); // ensure second element is "Labs"
+
+//I didn't change these because they are virtually the same as the built in assert function
+//I figured asserting an assert was strange.
